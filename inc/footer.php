04dff3c2b04ee2ef00649ac2a287e723a3060c75
robin <robin21101@gmail.com>
2016-10-20 12:47:57 +0900

    두번째세번째

--- inc/footer.php
+++ inc/footer.php
@@ -1,8 +1,3 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: r2
- * Date: 2016-10-20
- * Time: 오전 11:06
- */
+
 echo 'footer';
--- index.php
+++ index.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: r2
- * Date: 2016-10-20
- * Time: 오전 11:13
- */
 
 echo 'Hi';
 
