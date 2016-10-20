d70f2306bea89de8038563f7631d973a56331cb5
robin <robin21101@gmail.com>
2016-10-20 11:41:25 +0900

    '테스트'

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: r2
+ * Date: 2016-10-20
+ * Time: 오전 11:13
+ */
+
+echo 'Hi';
+
+echo '<a href="svc/index.php">test</a>';
---
+++ svc/index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: r2
+ * Date: 2016-10-20
+ * Time: 오전 10:59
+ */
